6310f04403c9edde8a3316f9f9604ead622e695e
Adam Duracz <adam@duracz.net>
2017-04-10 10:48:13 -0500

    autop

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 let package = Package(
     name: "TestLib",
     dependencies: [
-      .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 14),
-      .Package(url: "../CEnergymon", majorVersion: 1)
+      .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 14)
     ]
 )
